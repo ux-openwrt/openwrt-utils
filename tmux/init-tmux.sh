1892406a109f83eb8598b49ed0ec59eb1365de16
UxGood <uxgood.org@gmail.com>
2020-10-14 02:26:39 +0000

    tmux remove option monitor-bell
    Signed-off-by: UxGood <uxgood.org@gmail.com>

--- tmux/init-tmux.sh
+++ tmux/init-tmux.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 bindir=$(dirname $0)
+rcfile=~/.bashrc
+if [ -f $rcfile ]; then
+    sed -i '/^PS1=/d' $rcfile
+    echo "PS1='\[\e]0;\u@\h: \w\a\]\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >>$rcfile
+fi
 cp -f $bindir/tmux.conf ~/.tmux.conf
